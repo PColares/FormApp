a48821692b73f1b448769ab788c99c856aedc11f
Pedro Colares <pedro.colares@compasso.com.br>
2022-05-30 21:53:50 -0300

    Add screen change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import Form from './components/Form'
+import Form from './components/Form/Form'
 import data from './assets/data.png'
 
 function App() {
+  const [SucessState, setSucessState] = useState(true);
 
   const Title = "Intern Sign Up"
 
+  function handleState(SucessState) {
+    setSucessState(SucessState)
+  }
+
   return (
-    <>
+    <div className="App-Container">
+
       <div className="Container">
 
         <header className="App-header">
           <img src={data} className="App-logo" alt="data icon" />
-          <h1>{Title}</h1>
-          <Form />
+          {SucessState ? (
+            <>
+              <h1>{Title}</h1>
+              <button
+                onClick={() => {
+
+                  setSucessState(false)
+
+                }}
+              >
+                Back
+              </button>
+            </>
+          ) : <Form FormState={handleState} />}
         </header>
       </div>
-    </>
+    </div>
   )
 }
 
