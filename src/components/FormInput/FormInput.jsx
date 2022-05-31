ddcbb2b9db003732016aad136aa48961ae77b951
Pedro Colares <pedro.colares@compasso.com.br>
2022-05-31 07:25:27 -0300

    Fix FormInput onBlur props

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -2,11 +2,6 @@ import React, { useState } from 'react'
 import './FormInput.css'
 
 function FormInput({ variant, text, error, ...others }) {
-    const [focused, setFocused] = useState(false);
-
-    const handleFocus = (e) => {
-        setFocused(true);
-    };
 
     return (
         <>
@@ -16,9 +11,6 @@ function FormInput({ variant, text, error, ...others }) {
                 <input
                     className={variant === "short" ? "form-input-short" : "form-input-long"}
                     id={others.id}
-                    onBlur={handleFocus}
-                    onFocus={() => setFocused(true)}
-                    focused={focused.toString()}
                     {...others}
                 />
                 <span className="error">{error}</span>
